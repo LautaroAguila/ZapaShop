eed6703b66f172d57c83b4ab6f73ea83aa8ed9cf
Lautaro Aguilar <lautaroaguilar.c@gmail.com>
2023-12-23 15:40:02 -0300

    Primer preentrega proyecto

---
+++ src/Componentes/CartWidget/CartWidget.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import Carro from './Imagenes/carro.jpg'
+import "./CartWidget.css"
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faCartShopping} from '@fortawesome/free-solid-svg-icons'
+
+
+function CartWidget ({precio, nombre}){
+    return(
+        <div className='carro'>
+            <p className='numeroCarro'>
+                0
+            </p>
+            <FontAwesomeIcon icon={faCartShopping}/>
+            
+        </div>
+    )
+}
+
+export default CartWidget;
---
+++ src/Componentes/NavBar/NavBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './NavBar.css'
+import CartWidget from '../CartWidget/CartWidget'
+
+
+function NavBar (){
+    return(
+        <header>
+            <nav>
+                <div className="containerNav">
+                    <div className="logoContainer">
+                        <img src="src/logo/logo1.png" alt="" />
+                    </div>
+                    
+                    <div className="linksContainer">
+                        <a href="">Inicio</a>
+                        <a href="">Nosotros</a>
+                        <a href="">Contacto</a>
+                    </div>
+                    <CartWidget/>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
+
