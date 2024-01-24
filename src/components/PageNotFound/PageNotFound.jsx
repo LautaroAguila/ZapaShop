0af911cdb8856e4c5170fbaa69128f41bcdcf3bf
LautaroAguilar <lautaroaguilar.c@gmail.com>
2024-01-24 20:59:43 -0300

    pageNotFound

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './PageNotFound.css'
 
 const PageNotFound = () => {
     return (
-        <div>PageNotFound</div>
+        <div className='conteinerNotFound'>
+            <div className="container">
+                <h2>Lo siento! No se encontro la pagina.</h2>
+                <h1>404</h1>
+                <p>La pagina que busca aun no existe.</p>
+                <Link to='/' >Volver al Inicio</Link>
+            </div>
+        </div>
     )
 }
 
